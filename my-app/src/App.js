a74fddc29d4c64f0aaad92aa94fe0ec8278d022f
Ali Ibrahim Al-Sanabani <alsanabani971@gmail.com>
2023-10-07 13:58:45 +0300

    just save format

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -9,43 +9,29 @@ import Footer from "./components/footer/footer";
 import ScrollToTop from "./components/footer/scrolltotop";
 import Services from "./components/services";
 
-
 export default function App() {
+  return (
+    <>
+      <div className="container_of_form_popularlisting">
+        <Form />
+      </div>
+      <Footer />
+      <ScrollToTop />
 
+      {/* <Services /> */}
+    </>
 
+    // <div className="app-main">
 
-    return (
-        <>
+    //     <div className="top-part">
+    //         <Header />
+    //         <Nav />
 
+    //     </div>
+    //     <div className="Used_vechicles">
+    //         <h1>Used Vehicles For Sale </h1>
+    //     </div>
 
-            <div className="container_of_form_popularlisting">
-
-                <Form />
-
-
-            </div>
-            <Footer />
-            <ScrollToTop />
-
-            {/* <Services /> */}
-        </>
-
-
-
-        // <div className="app-main">
-
-        //     <div className="top-part">
-        //         <Header />
-        //         <Nav />
-
-        //     </div>
-        //     <div className="Used_vechicles">
-        //         <h1>Used Vehicles For Sale </h1>
-        //     </div>
-
-        // </div>
-
-
-
-    )
+    // </div>
+  );
 }
